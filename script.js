ab4ca99ff884f325fd255654ce5dad2ecf0374f9
cgscodes <67127238+cgscodes@users.noreply.github.com>
2024-02-14 16:46:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ window.addEventListener('scroll', () => {
     gateLeft.style.marginTop = value * 0.5 + 'px';
     gateRight.style.marginTop = value * 0.5 + 'px';
 });
+
+let lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for(list of lists){
+    list.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        });
+    })
+}
+
+window.addEventListener('click', () => {
+    document.getElementById("song").play();
+})
